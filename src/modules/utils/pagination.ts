47b8ed51bd224efe75f731d1bc5fc3a3d04650b8
cgiovanni <cgiovanni1996@gmail.com>
2020-02-11 21:36:47 +0100

    :recycle: refactor: added type for pagination utility

--- src/modules/utils/pagination.ts
+++ src/modules/utils/pagination.ts
@@ -1,4 +1,11 @@
-export default {
+export interface PaginationUtils {
+  hasMore: (total: number, skip: number, take: number) => boolean;
+  page: (skip: number, take: number) => number;
+}
+
+const pagination: PaginationUtils = {
   hasMore: (total: number, skip: number, take: number): boolean => (total - (skip + take)) > 0,
   page: (skip: number, take: number): number => (skip / take) + 1,
 };
+
+export default pagination;
