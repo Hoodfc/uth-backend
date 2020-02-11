8d93d3974ca9ace6751bee53085df858fa37fefe
cgiovanni <cgiovanni1996@gmail.com>
2020-02-11 14:56:38 +0100

    :recycle: feat: moved redis client into a separate file

--- src/modules/server/connection.ts
+++ src/modules/server/connection.ts
@@ -2,12 +2,12 @@ import * as http from 'http';
 import { ApolloServer } from 'apollo-server-express';
 import { createConnection } from 'typeorm';
 import * as express from 'express';
-import * as Redis from 'ioredis';
 import * as cors from 'cors';
 import context from './context';
 import Context from '../types/Context';
 import buildSchema from '../schema/buildSchema';
 import session from './session';
+import redis from './redis';
 
 export default async (
   PORT: string,
@@ -18,7 +18,6 @@ export default async (
 
   // express and redis declaration
   const app = express();
-  const redisClient = new Redis();
 
   // allow cors requests from client
   app.use(
@@ -29,7 +28,7 @@ export default async (
   );
 
   // create session through redis client
-  app.use(session(redisClient));
+  app.use(session(redis));
 
   const server = new ApolloServer({
     schema: await buildSchema(emitSchema),
