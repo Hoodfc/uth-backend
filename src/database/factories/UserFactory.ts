97ab6d2852ef9b2ee64fe2960a76387742749062
cgiovanni <cgiovanni1996@gmail.com>
2020-02-11 17:23:26 +0100

    :recycle: feat: random role in user seeder

--- src/database/factories/UserFactory.ts
+++ src/database/factories/UserFactory.ts
@@ -2,6 +2,7 @@ import Faker from 'faker';
 import { define } from 'typeorm-seeding';
 import { writeFileSync } from 'fs';
 import User from '../entity/User';
+import roles from '../../modules/constants/roles';
 
 define(User, (faker: typeof Faker) => {
   const name = faker.internet.userName();
@@ -12,7 +13,9 @@ define(User, (faker: typeof Faker) => {
   user.name = name;
   user.password = password;
   user.email = email;
-  console.log(user);
-  writeFileSync('src/database/logs/users.txt', `user: ${user.name}, password: ${password}, email: ${email} \n`, { flag: 'as' });
+  const roleIdx = Math.floor(Math.random() * 3);
+  const rolesArr = [roles.user, roles.admin, roles.author];
+  user.role = rolesArr[roleIdx];
+  writeFileSync('src/database/logs/users.txt', `user: ${user.name}, password: ${password}, email: ${email}, role: ${user.role} \n`, { flag: 'as' });
   return user;
 });
